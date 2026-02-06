74761b6f9b1bedb287b6fe07155bd493617072bd
Anastasia <anastasia.kio.oic@gmail.com>
2026-02-06 17:31:01 +0500

    Create EmptyLabyrinthPractice.cs

---
+++ Cycles/EmptyLabyrinthPractice.cs
@@ -0,0 +1,24 @@
+// Вставьте сюда финальное содержимое файла EmptyMazeTask.cs
+using System;
+using Mazes;
+
+public static class EmptyMazeTask
+{
+    public static void MoveOut(Robot robot, int width, int height)
+    {
+        MoveRight(robot, width - 3);
+        MoveDown(robot, height - 3);
+    }
+
+    private static void MoveRight(Robot robot, int steps)
+    {
+        for (int i = 0; i < steps; i++)
+            robot.MoveTo(Direction.Right);
+    }
+
+    private static void MoveDown(Robot robot, int steps)
+    {
+        for (int i = 0; i < steps; i++)
+            robot.MoveTo(Direction.Down);
+    }
+}
