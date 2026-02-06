83897969a673b94b951d58c33728877834cd8123
Anastasia <anastasia.kio.oic@gmail.com>
2026-02-06 17:15:01 +0500

    Create TheUltimateQuestion_of_the_Universe.cs

---
+++ First introduction to C#/TheUltimateQuestion_of_the_Universe.cs
@@ -0,0 +1,9 @@
+public static double GetSquare(double value)
+{
+    double square = Math.Pow(value, 2);
+    return square;
+}
+static void Print(double square)
+{
+    Console.WriteLine(square);
+}
